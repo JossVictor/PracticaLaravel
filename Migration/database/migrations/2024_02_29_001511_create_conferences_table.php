d7a859b7d0a9d8b30bf0c175868791246d529bbe
José Víctor Martínez <victormtz12010@hotmail.com>
2024-02-28 18:40:59 -0600

    commit inicial

---
+++ Migration/database/migrations/2024_02_29_001511_create_conferences_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('conferences', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('conference_id');
+            $table->unsignedBigInteger('superbowl_id');
+            $table->foreign('conference_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('conferences');
+    }
+};
